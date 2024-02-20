c611491fb9cbef45304ed76de01fc460546062b5
Your Name <esmael@localhost.localdomain>
2024-02-20 19:02:16 +0200

    new buble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,9 +4,18 @@
 /**
  * bubble_sort - sorts an array of integers in ascending order using Bubble Sort algorithm
  * @array: pointer to the array to be sorted
- * @sizeOfArray: the length of array or the size of array
+ * @size: the length of array or the size of array
  *
  * Return: None (void)
+ *
+ * Description:
+ *   This function implements the Bubble Sort algorithm to sort an array of integers
+ *   in ascending order.
+ * 
+ * Complexity:
+ *   - Best Case: O(n) - When the array is already sorted, and no swaps are needed.
+ *   - Average Case: O(n^2) - When elements are randomly distributed.
+ *   - Worst Case: O(n^2) - When the array is sorted in descending order.
  */
 void bubble_sort(int *array, size_t size)
 {
@@ -15,7 +24,8 @@ void bubble_sort(int *array, size_t size)
 
     for (i = 0; i < size; i++)
     {
-        int swappedArray = 0;
+        int swapped = 0;
+
         for (j = 0; j < size - i - 1; j++)
         {
             if (array[j] > array[j + 1])
@@ -23,11 +33,15 @@ void bubble_sort(int *array, size_t size)
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
-                swappedArray = 1;
-                printf("%d ", array[j]);
+                swapped = 1;
+                printf("%d ", array[j]); /*  Print array after each swap* */
             }
         }
-        if (!swappedArray)
+
+        /**  If no two elements were swapped in the inner loop,
+         then the array is already sorted 
+         */
+        if (!swapped)
         {
             break;
         }
