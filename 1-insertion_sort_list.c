7b50180594036153a150a356eb15ea6f19f0015f
Your Name <esmael@localhost.localdomain>
2024-02-20 21:00:25 +0200

    insertion swap

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,60 +1,57 @@
 #include "sort.h"
 
 /**
- * swap - Swap two nodes in a doubly linked list.
- * @h: Pointer to the head of the list.
- * @n1: First node to swap.
- * @n2: Second node to swap.
+ * swap - Swap two nodes in a listint_t doubly-linked list.
+ * @head: A pointer to the head of the doubly-linked list.
+ * @node1: A pointer to the first node to swap.
+ * @node2: The second node to swap.
  *
  * Description:
- *   This function swaps two nodes in a doubly linked list. It updates the
- *   pointers of the nodes and the head of the list if necessary.
+ *   This function swaps two nodes in a listint_t doubly-linked list. It updates
+ *   the pointers of the nodes and the head of the list if necessary.
  */
-void swap(listint_t **h, listint_t *n1, listint_t *n2)
+void swap(listint_t **head, listint_t **node1, listint_t *node2)
 {
-    if (n1->prev != NULL)
-        n1->prev->next = n2;
+    (*node1)->next = node2->next;
+    if (node2->next != NULL)
+        node2->next->prev = *node1;
+    node2->prev = (*node1)->prev;
+    node2->next = *node1;
+    if ((*node1)->prev != NULL)
+        (*node1)->prev->next = node2;
     else
-        *h = n2;
-
-    if (n2->next != NULL)
-        n2->next->prev = n1;
-
-    n1->next = n2->next;
-    n2->prev = n1->prev;
-    n1->prev = n2;
-    n2->next = n1;
+        *head = node2;
+    (*node1)->prev = node2;
+    *node1 = node2->prev;
 }
 
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
  *                       using the insertion sort algorithm.
- * @list: Pointer to the head of the doubly linked list.
+ * @list: A pointer to the head of a doubly-linked list of integers.
  *
  * Description:
  *   This function sorts a doubly linked list of integers in ascending order
  *   using the insertion sort algorithm. It iterates through the list,
- *   swapping adjacent nodes as needed to place each element in its correct
- *   position.
+ *   starting from the second node, comparing each node with its predecessors,
+ *   and swapping nodes as necessary to insert each node in its correct position.
  *   It prints the list after each swap operation.
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *sorted;
+    listint_t *current_node, *insertion_node, *temporary_node;
 
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
 
-    sorted = *list;
-    current = sorted->next;
-
-    while (current != NULL)
+    for (current_node = (*list)->next; current_node != NULL; current_node = temporary_node)
     {
-        while (current->prev != NULL && current->n < current->prev->n)
+        temporary_node = current_node->next;
+        insertion_node = current_node->prev;
+        while (insertion_node != NULL && current_node->n < insertion_node->n)
         {
-            swap(list, current->prev, current);
-            print_list((const listint_t *)*list); 
+            swap(list, &insertion_node, current_node);
+            print_list((const listint_t *)*list);
         }
-        current = current->next;
     }
 }
