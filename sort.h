5d1c647bdab86948f859d4701fa830132eca92bb
Your Name <esmael@localhost.localdomain>
2024-02-20 19:23:21 +0200

    list

--- sort.h
+++ sort.h
@@ -1,5 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
+#include <stdlib.h> 
 
 /**
  * struct listint_s - Doubly linked list node
@@ -13,10 +14,43 @@ typedef struct listint_s
     const int n;
     struct listint_s *prev;
     struct listint_s *next;
-} listint_t;
+} listint_t; 
+
+/**
+ * create_listint - Creates a doubly linked list from an array of integers
+ *
+ * @array: Array to convert to a doubly linked list
+ * @size: Size of the array
+ *
+ * Return: Pointer to the first element of the created list. NULL on failure
+ */
+listint_t *create_listint(const int *array, size_t size)
+{
+    listint_t *list;
+    listint_t *node;
+    int *tmp;
+
+    list = NULL;
+    while (size--)
+    {
+        node = malloc(sizeof(*node));
+        if (!node)
+            return (NULL);
+        tmp = (int *)&node->n;
+        *tmp = array[size];
+        node->next = list;
+        node->prev = NULL;
+        list = node;
+        if (list->next)
+            list->next->prev = list;
+    }
+    return (list);
+}
 
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
+
 
 #endif
